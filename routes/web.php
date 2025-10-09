031f1bf2c55096bd60f829bb4a52f16d1354c49a
Antonio2772 <atn.randr@gmail.com>
2025-10-09 21:12:37 +0300

    chore: User tables

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
 
-Route::get('/', function () {
+/*
+|--------------------------------------------------------------------------
+| Routes Web – Projet RH
+|--------------------------------------------------------------------------
+| Toutes les routes sont regroupées sous le préfixe "RH" pour cohérence.
+| Exemple : http://localhost:8000/RH/register
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/RH', function () {
     return view('welcome');
+})->name('rh.home');
+
+// Groupe pour les routes d’authentification
+Route::prefix('RH')->group(function () {
+
+    // Inscription candidat
+    Route::get('/register', [RegisterController::class, 'showForm'])
+        ->name('rh.register.form');
+
+    Route::post('/register', [RegisterController::class, 'store'])
+        ->name('rh.register.store');
 });
