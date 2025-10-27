36eff05c0df041787b0c5e1bd6cd6e708365f271
Antonio2772 <atn.randr@gmail.com>
2025-10-27 13:09:58 +0300

    feat: add Exports CSV for the RH functionalities

--- app/Models/Contrat.php
+++ app/Models/Contrat.php
@@ -26,4 +26,20 @@ class Contrat extends Model
     {
         return $this->belongsTo(Candidature::class, 'candidature_id');
     }
+
+    /**
+     * Vérifier si le contrat peut être renouvelé/modifié
+     */
+    public function peutEtreRenouvele()
+    {
+        return $this->renouvellement < 1;
+    }
+
+    /**
+     * Vérifier si le contrat a déjà été renouvelé
+     */
+    public function estDejaRenouvele()
+    {
+        return $this->renouvellement >= 1;
+    }
 }
